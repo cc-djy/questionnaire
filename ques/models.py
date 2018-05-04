b9786ba9afbe812fd143a4e9cf430c782711c7f9
niracler <1026037967@qq.com>
2018-05-04 22:54:36 +0800

    niracler现在弄了一种读数据库的方式，而不是读json文件的方式，不过使用了django的缓存机制

--- ques/models.py
+++ ques/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Ques(models.Model):
+    title = models.CharField(max_length=100)  # 标题,限定长度最多一百
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+
+    # 如果要将一个类的实例转化为str，则要定义__str__方法,相当于java中的toString
+    def __str__(self):
+        return self.title
--- ques/views.py
+++ ques/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
+from .models import Ques
+
 
 # Create your views here.
 
 
 @cache_page(60 * 15)  # 秒数，这里指缓存 15 分钟，不直接写900是为了提高可读性
 def index(request):
+    # 读取题目
     context = {}
+    context['topics'] = Ques.objects.all()
+
     # 读取数据库等 并渲染到网页
     return render(request, 'index.html', context)
