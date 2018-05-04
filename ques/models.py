3914dd7e53bb72d2ac3389fd155a8eb3eea7907f
niracler <1026037967@qq.com>
2018-05-04 23:10:30 +0800

    尴尬，前端不会

--- ques/models.py
+++ ques/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Ques(models.Model):
     title = models.CharField(max_length=100)  # 标题,限定长度最多一百
-    option1 = models.CharField(max_length=100)
-    option2 = models.CharField(max_length=100)
+    option1 = models.CharField(max_length=50)
+    option2 = models.CharField(max_length=50)
 
     # 如果要将一个类的实例转化为str，则要定义__str__方法,相当于java中的toString
     def __str__(self):
