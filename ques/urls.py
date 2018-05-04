0608a43dab351dc9c9951cda966e0d7175a70902
niracler <1026037967@qq.com>
2018-05-04 22:10:36 +0800

    好吧，开始分工了

--- ques/urls.py
+++ ques/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from ques import views
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
+    path('', views.index, name='index'),
 ]
--- ques/views.py
+++ ques/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 # Create your views here.
 
-def hello(request):
+
+@cache_page(60 * 15)  # 秒数，这里指缓存 15 分钟，不直接写900是为了提高可读性
+def index(request):
     context = {}
-    return render(request, "hello.html", context)
+    # 读取数据库等 并渲染到网页
+    return render(request, 'index.html', context)
