302664d5eb0feeb2e5bad9df78132ac08f9da966
will <844116221@qq.com>
2018-05-11 23:09:04 +0800

    更新

--- static/js/wenjuan.js
+++ static/js/wenjuan.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-    $('#bt').click(function() {
-        $(".errormessage").attr("display", "block")
+    var array = $("table");
+
+    $("#bt").click(function() {
+        for (var i = 0; i < array.size(); i++) {
+            var id = array[i].id
+            var val = $('input:radio[name="option' + (i + 1) + '"]:checked').val();
+            if (val == null) {
+                $("#" + id).css("border", "2px solid red");
+                $("#" + id + " .errormessage").css("display", "block");
+            }
+        }
+
+    })
+
+    $(":radio").click(function() {
+        var flag = $(this).parents("table").css("border");
+        if (flag != null) {
+            $(this).parents("table").css("border", "");
+            $(this).parents("tr").nextAll().last().css("display", "none");
+        }
+    })
+
+    $(function() {
+        var scroll_offset = $("")
     })
 })
