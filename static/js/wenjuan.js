b9159ea96cd1f6b793d56f020fc76f65b0f71019
will <844116221@qq.com>
2018-05-12 02:24:42 +0800

    更新

--- static/js/wenjuan.js
+++ static/js/wenjuan.js
@@ -2,26 +2,40 @@ $(document).ready(function() {
     var array = $("table");
 
     $("#bt").click(function() {
+        var j = 0;
+        var flag = true;
         for (var i = 0; i < array.size(); i++) {
             var id = array[i].id
             var val = $('input:radio[name="option' + (i + 1) + '"]:checked').val();
             if (val == null) {
-                $("#" + id).css("border", "2px solid red");
-                $("#" + id + " .errormessage").css("display", "block");
+                if (flag) {
+                    j = i;
+                    flag = false;
+                }
+                $("#" + id).css("border-color", "red");
+                $("#" + id + " .errormessage").css("visibility", "visible");
             }
         }
+        var scroll_offset = $("#" + array[j].id).offset(); //得到pos这个div层的offset，包含两个值，top和left 
+        $("body,html").animate({
+            scrollTop: scroll_offset.top //让body的scrollTop等于pos的top，就实现了滚动 
+        }, 200);
+        $(".scrolltop").show();
+        $(".scrolltop").click(function() {
+            $("html, body").animate({
+                scrollTop: $(document).height()
+            }, 200);
+            $(".scrolltop").hide();
+        });
 
-    })
+    });
 
     $(":radio").click(function() {
         var flag = $(this).parents("table").css("border");
         if (flag != null) {
-            $(this).parents("table").css("border", "");
-            $(this).parents("tr").nextAll().last().css("display", "none");
+            $(this).parents("table").css("border-color", "#F7F7F7");
+            $(this).parents("tr").nextAll().last().css("visibility", "hidden");
         }
-    })
+    });
 
-    $(function() {
-        var scroll_offset = $("")
-    })
 })
