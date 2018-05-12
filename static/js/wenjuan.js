efdf472fed111f451be1216ccdf430463d5946ce
will <844116221@qq.com>
2018-05-12 17:40:27 +0800

    更新

--- static/js/wenjuan.js
+++ static/js/wenjuan.js
@@ -1,24 +1,31 @@
 $(document).ready(function() {
     $("#part2 table tr>th:even").css("background-color", "#F9F9F9");
     $("#part2 table tr>th:odd").css("background-color", "#FFFFFF");
-    var array = $("#part1 table,#part3 table");
+    var array = $("#part1 table,#part2 table tr,#part3 table");
+    alert(array.size());
     $("#bt").click(function() {
-        var j = 0;
+        var m = 0;
         var flag = true;
         for (var i = 0; i < array.size(); i++) {
-            var id = array[i].id
+            var id = array[i].id;
             var val = $('input:radio[name="option' + (i + 1) + '"]:checked').val();
             if (val == null) {
                 if (flag) {
-                    j = i;
+                    m = i;
                     flag = false;
                 }
-                $("#" + id).css("border-color", "red");
-                $("#" + id + " .errormessage").css("visibility", "visible");
+                if ($("#" + id).prop("tagName") == "TR") {
+                    $("#" + id).css("border-color", "red");
+                    $("#" + id).prev().css("border-bottom-color", "red");
+                } else {
+                    $("#" + id).css("border-color", "red");
+                    $("#" + id + " .errormessage").css("visibility", "visible");
+                }
             }
         }
+
         if (!flag) {
-            var scroll_offset = $("#" + array[j].id).offset(); //得到pos这个div层的offset，包含两个值，top和left 
+            var scroll_offset = $("#" + array[m].id).offset(); //得到pos这个div层的offset，包含两个值，top和left 
             $("body,html").animate({
                 scrollTop: scroll_offset.top //让body的scrollTop等于pos的top，就实现了滚动 
             }, 200);
@@ -33,10 +40,18 @@ $(document).ready(function() {
     });
 
     $(":radio").click(function() {
-        var flag = $(this).parents("table").css("border");
-        if (flag != null) {
-            $(this).parents("table").css("border-color", "#F7F7F7");
-            $(this).parents("tr").nextAll().last().css("visibility", "hidden");
+        alert($(this).parents("#part2"));
+        if (1 != null) {
+            alert(132);
+            $(this).css("border-color", "#F7F7F7");
+            $(this).prev().css("border-bottom-color", "#F7F7F7");
+        } else {
+            alert(556);
+            var flag = $(this).parents("table").css("border");
+            if (flag != null) {
+                $(this).parents("table").css("border-color", "#F7F7F7");
+                $(this).parents("tr").nextAll().last().css("visibility", "hidden");
+            }
         }
     });
 
